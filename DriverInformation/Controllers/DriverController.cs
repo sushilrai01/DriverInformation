c88070390485aafbc139ba531f599878fdaeeb2e
sushilrai01 <ssilray16@gmail.com>
2023-09-05 16:41:04 +0545

    Add project files.

---
+++ DriverInformation/Controllers/DriverController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using DriverInformation.Models;
+using DriverInformation.ViewModel;
+
+namespace DriverInformation.Controllers
+{
+
+    public class DriverController : Controller
+    {
+        private DriverManagementEntities db = new DriverManagementEntities();
+        // GET: Driver
+        public ActionResult Index()
+        {
+            var DriverInfo = (from drivertbl in db.DriverTables
+                              join gendertbl in db.GenderTables on drivertbl.GenderId equals gendertbl.GenderId
+                              join activitytbl in db.ActivityTables on drivertbl.IsActive equals activitytbl.IsActive
+                              join hobbytbl in db.HobbyTables on drivertbl.HobbyId equals hobbytbl.HobbyId
+                              select new DriverInfoModel
+                              {
+                                  DriverId = drivertbl.DriverId,
+                                  DriverName = drivertbl.Name,
+                                  ContactNo = drivertbl.ContactNo,
+                                  Category = gendertbl.Category,
+                                  Hobby = hobbytbl.Hobby,
+                                  Available = activitytbl.Available,
+                              }).ToList();
+            return View(DriverInfo);
+        }
+
+        //GET: Driver/Create
+        public ActionResult Create()
+        {
+           DriverInfoModel model = new DriverInfoModel();
+           model.Dropdownlist = db.GenderTables
+                                .Select(x => new DropdownModel { ID = x.GenderId, TEXT = x.Category }).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ DriverInformation/ViewModel/DriverViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DriverInformation.ViewModel
+{
+    //public class DriverViewModel
+    //{
+    //}
+
+    public class GenderModel
+    {
+        public int GenderId{ get; set; }
+        public string Category { get; set; }
+    }
+
+    public class ActivityModel
+    {
+        public int ActiveId { get; set; }
+        public string Available { get; set; }
+    }
+
+    public class HobbyModel
+    {
+        public int HobbyId { get; set; }
+        public string Hobby { get; set; }
+    }
+    public class DropdownModel
+    {
+        public int ID { get;set; } 
+        public string TEXT { get;set; }
+    }
+
+    public class DriverInfoModel
+    {
+        public int DriverId { get; set; }
+        public string DriverName { get; set; }
+        public string ContactNo { get; set; }
+        public int GenderId { get; set; }
+        public string Category { get; set; }
+        public int ActiveId { get; set; }
+        public string Available { get; set; }
+        public int HobbyId { get; set; }
+        public string Hobby { get; set; }
+        
+        public List<DropdownModel> Dropdownlist { get; set; }
+    }
+
+}
